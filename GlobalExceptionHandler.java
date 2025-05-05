b3b7e36e823d6ad7dc6f1090210659393611ab2b
sanjivanibabalsure <128891523+sanjivanibabalsure@users.noreply.github.com>
2025-05-05 12:49:40 +0530

    Add files via upload

---
+++ GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneral(Exception ex) {
+        return new ResponseEntity<>("Something went wrong: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
